6227af1821ae72f2daa250b8feef2f89867fd8a2
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-01-18 19:57:10 -0600

    adding header component

---
+++ frontend/src/components/header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import classes from './css/header.module.css'
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+   const user = {
+        name:"john",
+   };
+
+   const cart = {
+    totalCount: 10,
+   };
+
+    const logout = () => {}
+    
+    return (
+        <header className={classes.header}>
+            <div className={classes.container}>
+                <Link to="/" className={classes.logo}>
+                    Fire FIts
+                </Link>
+                <nav>
+                    <ul>
+                        {
+                            user?(
+                            <li className={classes.menu_container}>
+                                <Link to="/profile">{user.name}</Link>
+                                <div className={classes.menu}>
+                                    <Link to="/profile">Profile</Link>
+                                    <Link to="/orders">Orders</Link>
+                                    <a onClick={logout}>Logout</a>
+                                </div>
+                            </li>
+                            ) : (
+                            <Link to="/login">Login</Link>
+                        )}
+                    </ul>
+                </nav>
+
+            </div>
+        </header>
+    );
+}
